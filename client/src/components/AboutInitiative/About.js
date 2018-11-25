2006b222811dbcf6c05ff28df4a675ebc76b25c2
saqibarfeen <saqib.arfeen@gmail.com>
2018-11-25 08:41:14 +0500

    Created the about the initiative page

---
+++ client/src/components/AboutInitiative/About.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography'
+
+const styles = {
+  card: {
+    maxWidth: 345,
+  },
+  media: {
+    height: 140,
+  },
+};
+
+class About extends Component {
+    constructor(){
+        super()
+        this.state={
+
+        }
+    }
+
+  render() {
+    const x= this.props;
+    return (
+      <Card className={styles.card}>
+      <CardActionArea>
+        <img src={x.img} alt={x.name}/>
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">
+            {x.name}
+          </Typography>
+          <Typography component="p">
+           {x.content}
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+        <Button size="small" color="primary">
+          Learn More
+        </Button>
+      </CardActions>
+    </Card>
+
+      );  }
+}
+
+export default About;
---
+++ client/src/components/AboutInitiative/README.md
@@ -0,0 +1,6 @@
+
+##To add this page in your app, just add the MainPage component in your route (without any props).
+#dependencies: @material-ui/core
+
+e.g import MainPage from './components/AboutInitiative/MainPage';
+    <MainPage/>
