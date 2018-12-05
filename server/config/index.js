1ee88f9bd19e8f006cb8f069b44806c5ce3b3086
zeeshanhanif <zeeshanhanif@gmail.com>
2018-12-06 02:25:43 +0500

    Integrated twilio API and created routes form phone number and verfication code

--- server/config/index.js
+++ server/config/index.js
@@ -7,6 +7,8 @@ exports = module.exports = function(app) {
     app.set("cloud_name",'dgtzrxfyd');
     app.set("api_key",'893115935379578');
     app.set("api_secret",'lWY5FkcFfahA-oaU8KFwD2buQDY');
+    app.set("authy_api_key",'JjqCzj1G771Xqn5cAR70f1HIN2aROl5a');
+    app.set("showLogs",true);
 
     // Use for production
     //app.set("mongodb-url","mongodb://user:pass@id.mongolab.com:port/database");
